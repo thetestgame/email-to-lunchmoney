5ee42f993a4cb2c1cc122d329cb3dc3d5bd94054
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-07-28 11:09:47 -0400

    Use null for notes match

--- src/lunchmoney.ts
+++ src/lunchmoney.ts
@@ -66,7 +66,7 @@ export async function processActions(env: Env) {
       .reverse()
       .find(
         (txn: any) =>
-          txn.notes === '' &&
+          txn.notes === null &&
           txn.payee === action.match.expectedPayee &&
           txn.amount === (action.match.expectedTotal / 100).toFixed(4)
       );
